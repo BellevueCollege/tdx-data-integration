59b8421352a197689cbe1416d4b61721d6ffb9ab
Nicole Swan <nicole.swan@bellevuecollege.edu>
2016-12-05 18:13:13 -0800

    Add student file generation logic
    - This is a terrible atomic commit (as in not), forgive me coding lords
    - Adds functionality for student file generation
    - Adds improved logging

--- file-diff-import.py
+++ file-diff-import.py
@@ -6,8 +6,14 @@ import logging
 
 import config
 
-logging.basicConfig(level=logging.INFO) #set logging level
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s %(module)s %(levelname)s: %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO) #set logging format
 
+'''
+ Functions used in script
+'''
+# write worksheet headers
 def write_sheet_headers(worksheet, headers):
     write_col = 0
     for header in headers:
@@ -15,30 +21,57 @@ def write_sheet_headers(worksheet, headers):
         write_col += 1
     return
 
+# function to increment column number
 def increment_column():
     global write_col
     write_col += 1
     return write_col
+''' 
+End functions used
+'''
 
+# Set argument info
 parser = argparse.ArgumentParser()
-parser.add_argument('--type', help='add or update. Default is update.')
+parser.add_argument('--filetype', help='add or update. Default is update.')
+parser.add_argument('--usertype', help='employee or student. Default is employee.')
 args = parser.parse_args()
 
-#print args.type
+# Set import file type
 file_type = config.filetype_update
-if args.type is not None and args.type in [config.filetype_add, config.filetype_update]:
-    file_type = args.type
+if args.filetype is not None and args.filetype in [config.filetype_add, config.filetype_update]:
+    file_type = args.filetype
+else:
+    logger.warn("Invalid filetype provided. Default value used.")
+
+# Set file user type
+user_type = config.usertype_employee
+if args.usertype is not None and args.usertype in [config.usertype_employee, config.usertype_student]:
+    user_type = args.usertype
+else:
+    logger.warn("Invalid usertype provided. Default value used.")
 
 today = datetime.date.today()
 #print file_type
 #print today
+#print user_type
 
-workbook = xlsxwriter.Workbook('employee-'+ file_type + '-' + 'import-' + str(today) + '.xlsx')
+file_name = user_type + '-' + file_type + '-import-' + str(today) + '.xlsx'
+workbook = xlsxwriter.Workbook(file_name)
 worksheet = workbook.add_worksheet()
 
-add_headers = ['User Type','Username','Authentication Provider','Authentication Username','Security Role','First Name','Last Name','Organization','Title','Acct/Dept','Organizational ID','Is Employee','Primary Email','Alert Email','Work Phone','Work Address','Work Postal Code','Time Zone ID','HasTDKnowledgeBase','HasTDRequests','HasTDTicketRequests','Department ID','Is Student']
-update_headers = ['Username','Authentication Username','First Name','Last Name','Organization','Title','Acct/Dept','Organizational ID','Is Employee','Primary Email','Alert Email','Work Phone','Work Address','Work Postal Code','Time Zone ID','Reports To Username','Department ID', 'Is Student']
+add_headers = ['User Type','Username','Authentication Provider','Authentication Username','Security Role','First Name','Last Name','Organization','Title','Acct/Dept','Organizational ID','Is Employee','Primary Email','Alert Email','Work Phone','Work Postal Code','Time Zone ID','HasTDKnowledgeBase','HasTDRequests','HasTDTicketRequests','Is Student']
+update_headers = ['Username','Authentication Username','First Name','Last Name','Organization','Title','Acct/Dept','Organizational ID','Is Employee','Primary Email','Alert Email','Work Phone','Work Address','Work Postal Code','Time Zone ID','Is Student']
+headers_employee_only = ['Work Address','Department ID','Reports To Username']
 
+
+# Add headers specific to employe user type
+if user_type == config.usertype_employee:
+    if file_type == config.filetype_add:
+        add_headers = add_headers + headers_employee_only
+    elif file_type == config.filetype_update:
+        update_headers = update_headers + headers_employee_only
+
+# write worksheet headers
 if file_type == config.filetype_add:
     write_sheet_headers(worksheet, add_headers)
 else:
@@ -47,9 +80,14 @@ else:
 try:
     with pymssql.connect(server=config.server,user=config.user,password=config.password,database=config.database) as conn:
         with conn.cursor(as_dict=True) as cursor:
-            #cursor.execute('SELECT * FROM Forms')
-            cursor.execute('SELECT TOP 100 * from vw_Employees')
 
+            # select the correct data
+            if user_type == config.usertype_employee:
+                cursor.execute('SELECT * from vw_Employees')
+            else:
+                cursor.execute('SELECT * from vw_Students')
+
+            # process the data
             write_row = 1
             for row in cursor:
                 write_col = 0
@@ -76,26 +114,32 @@ try:
                 worksheet.write(write_row, increment_column(), row['Primary Email'])
                 worksheet.write(write_row, increment_column(), row['Alert Email'])
                 worksheet.write(write_row, increment_column(), row['Work Phone'])
-                worksheet.write(write_row, increment_column(), row['Work Address'])
                 worksheet.write(write_row, increment_column(), row['Work Postal Code'])
                 worksheet.write(write_row, increment_column(), row['Time Zone ID'])
-
-                if file_type == config.filetype_update:
-                    worksheet.write(write_row, increment_column(), 'Reports to Username')
 
                 if file_type == config.filetype_add:
                     worksheet.write(write_row, increment_column(), 'T')  #HasTDKnowledgeBase
                     worksheet.write(write_row, increment_column(), 'T')  #HasTDRequests
                     worksheet.write(write_row, increment_column(), 'T')  #HasTDTicketRequests
 
-                worksheet.write(write_row, increment_column(), row['Department ID'])
                 worksheet.write(write_row, increment_column(), row['Is Student'])
+
+                # add columns that apply only to employees
+                if user_type == config.usertype_employee:
+                    worksheet.write(write_row, increment_column(), row['Work Address'])
+                    worksheet.write(write_row, increment_column(), row['Department ID'])
+
+                    if file_type == config.filetype_add:
+                        worksheet.write(write_row, increment_column(), '')
+                    else:
+                        worksheet.write(write_row, increment_column(), row['Reports to Username'])
+
                 write_row += 1
 
 except Exception, e:
     #print "Error ", sys.exc_info()[0].message
     print "Error: ", e.args[0]
-    logging.exception("Error connecting to db or writing to worksheet")
+    logger.exception("Error connecting to db or writing to worksheet")
 
 workbook.close()
 #print dir(conn)
