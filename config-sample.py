8454c4791cefd3a98db3a2f8bd16b9e28370363e
Nicole Swan <nicole.swan@bellevuecollege.edu>
2016-12-05 18:12:39 -0800

    Update sample config file

--- config-sample.py
+++ config-sample.py
@@ -3,5 +3,10 @@ user = ''   #db user
 password = ''   #db password
 database = ''   #database to connect to
 
+#file type options
 filetype_add = 'add'
 filetype_update = 'update'
+
+#user type options
+usertype_employee = 'employee'
+usertype_student = 'student'
