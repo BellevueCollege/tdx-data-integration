05d366284f82321883929814179a0fdf60ee8d20
Nicole Swan <nicole.swan@bellevuecollege.edu>
2016-12-07 14:07:42 -0800

    Updates to the sample config

--- config-sample.py
+++ config-sample.py
@@ -10,3 +10,7 @@ filetype_update = 'update'
 #user type options
 usertype_employee = 'employee'
 usertype_student = 'student'
+
+# TeamDynamix web services admin keys
+tdx_web_services_beid = ''
+tdx_web_services_key = ''
