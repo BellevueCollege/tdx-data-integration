762cc99e76090dc2403e2cea0609914640af2bf5
Nicole Swan <nicole.swan@bellevuecollege.edu>
2016-12-21 16:39:45 -0800

    Update sample config file
    - Update TDX web services config
    - Add static variables
    - Add user splits variable

--- config-sample.py
+++ config-sample.py
@@ -11,6 +11,16 @@ filetype_update = 'update'
 usertype_employee = 'employee'
 usertype_student = 'student'
 
-# TeamDynamix web services admin keys
+# TeamDynamix web services config
+tdx_web_api_root = ''
 tdx_web_services_beid = ''
 tdx_web_services_key = ''
+
+#static variables
+tdx_default_securityrole_name = ''
+tdx_group_name_employee = ''    #Group name for employees group in TDX
+tdx_acct_name_employee = ''     #Default acct/dept name for employees
+tdx_acct_name_student = ''      #Default acct/dept name for students
+
+#user splits available
+user_splits_student = ['A-Da-d*', 'D-Id-i', 'J-Mj-m', 'N-Sn-s', 'T-Zt-z']
