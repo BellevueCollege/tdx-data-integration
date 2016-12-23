82a1d65ccb04328b53f82f5e3298abab6b52e6ad
Nicole Swan <nicole.swan@bellevuecollege.edu>
2016-12-23 09:50:35 -0700

    Add class for a new user

---
+++ newuser.py
@@ -0,0 +1,34 @@
+import json
+
+class NewUser():
+
+    def __init__(self):
+        self.usertype = 1   #User
+        self.applications = ['TDKnowledgeBase', 'TDRequests', 'TDTicketRequests']
+        self.timezone = 6   #Pacific
+        self.organization = 'Bellevue College'
+
+    def dictify(self):
+        data = {}
+        data['TypeID'] = self.usertype
+        data['Username'] = self.username
+        data['AuthenticationUserName'] = self.authusername
+        data['SecurityRoleID'] = self.securityrole
+        data['FirstName'] = self.firstname
+        data['LastName'] = self.lastname
+        data['Company'] = self.organization
+        data['Title'] = self.title
+        data['DefaultAccountID'] = self.accountid
+        data['ExternalID'] = self.orgid
+        data['IsEmployee'] = self.isemployee
+        data['PrimaryEmail'] = self.primaryemail
+        data['AlertEmail'] = self.alertemail
+        data['WorkPhone'] = self.workphone
+        data['WorkZip'] = self.workpostal
+        data['TZID'] = self.timezone
+        data['Applications'] = self.applications
+        data['Attributes'] = self.attributes
+        data['WorkAddress'] = self.workaddress
+        data['ReportsToUID'] = self.reportsto
+
+        return data
