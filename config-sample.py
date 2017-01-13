fdb0435ca5288517ef1c7fe551d2232dd5346c2c
Nicole Swan <nicole.swan@bellevuecollege.edu>
2017-01-13 14:38:34 -0800

    Add updated sample config file

--- config-sample.py
+++ config-sample.py
@@ -15,12 +15,19 @@ usertype_student = 'student'
 tdx_web_api_root = ''
 tdx_web_services_beid = ''
 tdx_web_services_key = ''
+tdx_web_use_sandbox = True
 
 #static variables
-tdx_default_securityrole_name = ''
-tdx_group_name_employee = ''    #Group name for employees group in TDX
-tdx_acct_name_employee = ''     #Default acct/dept name for employees
-tdx_acct_name_student = ''      #Default acct/dept name for students
+tdx_default_securityrole_name = ''  #Default security role for users
+tdx_group_name_employee = ''        #Group name for employees group in TDX
+tdx_acct_name_employee = ''         #Default acct/dept name for employees
+tdx_acct_name_student = ''          #Default acct/dept name for students
+tdx_component_id_person = 31        #Component ID for TDX person module
+tdx_customattr_isstudent_name = ''  #Name of Is Student custom attribute
+tdx_customattr_deptid_name = ''     #Name of Department ID custom attribute
 
-#user splits available
-user_splits_student = ['A-Da-d*', 'D-Id-i', 'J-Mj-m', 'N-Sn-s', 'T-Zt-z']
+#user splits
+user_splits_student = ['A-Ca-c*', 'D-Id-i', 'J-Lj-l', 'M-Qm-q', 'R-Sr-s', 'T-Zt-z'] #splits student pulls into digestible chunks
+
+#groups info for groups that should be synced in update-groups.py
+groups_to_sync = []                 #TDX groups to add/remove people to/from based on a TDX field
